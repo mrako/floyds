518c0da7c62434ba7b650a7908a889b663a1d1ed
Marko Klemetti <marko.klemetti@gmail.com>
2014-09-05 00:53:35 +0300

    initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('floyds.controllers', [])
+
+.controller('CardsCtrl', function($scope, Cards) {
+  var questionTypes = Cards.random().questions;
+
+  var addQuestion = function() {
+    var index = Math.floor(Math.random() * questionTypes.length);
+    $scope.questions.push(questionTypes[index]);
+    questionTypes.splice(index, 1);
+  }
+
+  $scope.questions = [];
+  addQuestion();
+
+  $scope.cardDestroyed = function(index) {
+    $scope.questions.splice(index, 1);
+  };
+
+  $scope.cardSwiped = function(index) {
+    addQuestion();
+  };
+})
+
+.controller('QuestionsCtrl', function($scope, $stateParams, Cards) {
+  $scope.card = Cards.get($stateParams.questionId);
+});
