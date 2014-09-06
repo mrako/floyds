ecfc9997add4cfa9f9bb15dddae91aabbd1d9464
Marko Klemetti <marko.klemetti@gmail.com>
2014-09-06 12:58:11 +0300

    Add questions

--- www/js/seeds.js
+++ www/js/seeds.js
@@ -75,7 +75,7 @@ var cards = [{
     "answer": "Tea",
     "category": "d"
   }, {
-    "title": "Tho whom is the quotation ''An army marches on it's stomach' attributed?",
+    "title": "Tho whom is the quotation An army marches on it's stomach' attributed?",
     "answer": "Napoleaon Bonaparte",
     "category": "t"
   }]
@@ -105,6 +105,249 @@ var cards = [{
     "title": "What is 'the very spice of life'?",
     "answer": "Variety (William Cowper)",
     "category": "t"
+  }]
+}, {
+  "id": 5,
+  "questions": [{
+    "category": "ss",
+    "title": "What is a Huffkin?",
+    "answer": "A kind of bread roll."
+  }, {
+    "category": "mc",
+    "title": "How many people will one quail serve?",
+    "answer": "One person"
+  }, {
+    "category": "v",
+    "title": "In the children's TV programme 'The Herbs' what was Parsley?",
+    "answer": "A very gentle lion."
+  }, {
+    "category": "sd",
+    "title": "Which milk pudding is an ingredient of Zurich Easter Cake?",
+    "answer": "Semolina."
+  }, {
+    "category": "d",
+    "title": "What is Kahlua?",
+    "answer": "A coffee liqueur."
+  }, {
+    "category": "t",
+    "title": "Which is the odd one out? Kipper, rollmop, carp, bloater.",
+    "answer": "Only carp is not prepared from a herring."
+  }]
+}, {
+  "id": 6,
+  "questions": [{
+    "category": "ss",
+    "title": "What kind of staple food was Manchet?",
+    "answer": "A type of bread"
+  }, {
+    "category": "mc",
+    "title": "How is Meunière butter prepared?",
+    "answer": "It is the butter in which fish has been fried."
+  }, {
+    "category": "v",
+    "title": "On Burns night, which vegetables are served with the Haggis?",
+    "answer": "Swedes and potatoes."
+  }, {
+    "category": "sd",
+    "title": "Which nuts flavour a Flan Lorrain?",
+    "answer": "Almonds"
+  }, {
+    "category": "d",
+    "title": "Which ingredients is indicated by the word kiss in a cocktail?",
+    "answer": "Cream"
+  }, {
+    "category": "t",
+    "title": "Which is the odd one out? Herring, Mackerel, Whiting.",
+    "answer": "Whiting is a white fish, the others are oily fishes."
+  }]
+}, {
+  "id": 7,
+  "questions": [{
+    "category": "ss",
+    "title": "What kind of food could be described as a flitch?",
+    "answer": "Bacon."
+  }, {
+    "category": "mc",
+    "title": "What term is used for a crab prepared and ready for eating?",
+    "answer": "Dressed."
+  }, {
+    "category": "v",
+    "title": "What is generally reckoned to be the most expensive way to eat potatoes?",
+    "answer": "As crisps."
+  }, {
+    "category": "sd",
+    "title": "What is the topping of a Welsh Border Tart?",
+    "answer": "Meringue."
+  }, {
+    "category": "d",
+    "title": "Which yellow liqueuer is added to a Harvey Wallbanger?",
+    "answer": "Galliano."
+  }, {
+    "category": "t",
+    "title": "Which is the odd one out? Pershore, Kirke's Blue, Spartan.",
+    "answer": "A Spartan is an apple, the others are plums."
+  }]
+}, {
+  "id": 8,
+  "questions": [{
+    "category": "ss",
+    "title": "What is added to fish in a fishcake?",
+    "answer": "Mashed potato."
+  }, {
+    "category": "mc",
+    "title": "When choosing beef from the buther's, which would be the best, bright red or plum red in colour?",
+    "answer": "Plum red would be the tenderest."
+  }, {
+    "category": "v",
+    "title": "Which type of onion is associated with Wales?",
+    "answer": "The leek."
+  }, {
+    "category": "sd",
+    "title": "Sacher Torte is a speciality of which town?",
+    "answer": "Vienna."
+  }, {
+    "category": "d",
+    "title": "Which liqueur is added to gin in an Alaska cocktail?",
+    "answer": "Yellow Chartreuse."
+  }, {
+    "category": "t",
+    "title": "Which is the odd one out and why, Egremont Russet, Packham's Triumph, Laxton's Superb?",
+    "answer": "Packham's Triumph is a pear, the others are apples."
+  }]
+}, {
+  "id": 9,
+  "questions": [{
+    "category": "ss",
+    "title": "Bradenham ham is distinguished by it's skin of what colour?",
+    "answer": "Black."
+  }, {
+    "category": "mc",
+    "title": "Which meat is used in a wilted salad?",
+    "answer": "Bacon."
+  }, {
+    "category": "v",
+    "title": "For about how long should you cook fresh pasta (to within a minute either way)?",
+    "answer": "2 minutes."
+  }, {
+    "category": "sd",
+    "title": "Miniature Ice Mountains are traditionally served in which country?",
+    "answer": "China."
+  }, {
+    "category": "d",
+    "title": "Creme de Noyeau is a liqueur tasting of what?",
+    "answer": "Almonds."
+  }, {
+    "category": "t",
+    "title": "For which food is Rhode Island, USA famous?",
+    "answer": "Poultry."
+  }]
+}, {
+  "id": 10,
+  "questions": [{
+    "category": "ss",
+    "title": "What term is used for bacon salted but not smoked?",
+    "answer": "Green bacon."
+  }, {
+    "category": "mc",
+    "title": "Bigos, containing sauerkraut and pork is the national dish of which country?",
+    "answer": "Poland."
+  }, {
+    "category": "v",
+    "title": "Which four herbs are mentioned in the song Scarborough Fair?",
+    "answer": "Parsley, sage, rosemary and thyme."
+  }, {
+    "category": "sd",
+    "title": "What is traditionally eaten on Shrove Tuesday?",
+    "answer": "Pancakes."
+  }, {
+    "category": "d",
+    "title": "Why is the Union Jack cocktail so called?",
+    "answer": "It is presented in three different coloured layers."
+  }, {
+    "category": "t",
+    "title": "When rice is grown in flooded paddy fields, how is it harvested?",
+    "answer": "Mechanically - the field is drained first."
+  }]
+}, {
+  "id": 11,
+  "questions": [{
+    "category": "ss",
+    "title": "What was Frumenty?",
+    "answer": "A form of porridge."
+  }, {
+    "category": "mc",
+    "title": "What would you be eating if an Italian served you with Pollo?",
+    "answer": "Chicken."
+  }, {
+    "category": "v",
+    "title": "What effect does overcooking have on corn on the cob?",
+    "answer": "It toughens it."
+  }, {
+    "category": "sd",
+    "title": "From which plant grown in Britain do we obtain sugar?",
+    "answer": "Sugar beet."
+  }, {
+    "category": "d",
+    "title": "Which spirit is the base of a Balaika cocktail?",
+    "answer": "Vodka."
+  }, {
+    "category": "t",
+    "title": "In which century was sugar first extracted from sugar beet - 18th, 19th or 20th century?",
+    "answer": "18th."
+  }]
+}, {
+  "id": 12,
+  "questions": [{
+    "category": "ss",
+    "title": "At what time of day would you eat oatcakes?",
+    "answer": "At breakfast time."
+  }, {
+    "category": "mc",
+    "title": "What kind of dish is the Greek Souvlakia?",
+    "answer": "A lamb kebab."
+  }, {
+    "category": "v",
+    "title": "In which country did chutney originate?",
+    "answer": "India."
+  }, {
+    "category": "sd",
+    "title": "Nae two of the three English counties in which Stilton is made?",
+    "answer": "Leicestershire, Derbyshire and Nottinghamshire."
+  }, {
+    "category": "d",
+    "title": "Vermouth and bitters are added to what, to make a Harvard cocktail?",
+    "answer": "Brandy."
+  }, {
+    "category": "t",
+    "title": "Give a food-related term for an intellectual?",
+    "answer": "Egg-head."
+  }]
+}, {
+  "id": 13,
+  "questions": [{
+    "category": "ss",
+    "title": "In which islands might you be served bacon with bananas for breakfast?",
+    "answer": "In the Caribbean Islands."
+  }, {
+    "category": "mc",
+    "title": "Members of Parliament 100 years ago would have feasted on which little fish at the end of a session?",
+    "answer": "Whitebait - locally caught."
+  }, {
+    "category": "v",
+    "title": "Which two cold cooked vegetables are contained in Salade Niçoise?",
+    "answer": "Potatoes and French beans."
+  }, {
+    "category": "sd",
+    "title": "Which sweetened dough cakes are 'dunked' in coffee in America?",
+    "answer": "Doughnuts."
+  }, {
+    "category": "d",
+    "title": "Why did parliament first introduce pub. Licencing hours?",
+    "answer": "To stop munitions workers from coming in drunk (or not at all)!"
+  }, {
+    "category": "t",
+    "title": "Where in London did Dr Johnson often eat and drink?",
+    "answer": "At the Cheshire Cheese, at the end of Fleet Street."
   }]
 }];
 
